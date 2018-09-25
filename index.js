26a290fb6cd365061129a8a420161ded9e2a3fd6
Beatriz <bia_nonato@icloud.com>
2018-09-25 19:34:32 -0300

    corrigir formula de juros e condições de emprestimo

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ let idade = Number(prompt("Informe sua idade:"));
 let renda = Number(prompt("Informe sua renda:"));
 let emprestimo = Number(prompt("Informe valor do empréstimo:"));
 
-if(idade < 21 || idade > 55 || renda < 1000 || emprestimo < 500 || emprestimo >= renda * 15){
+if(idade < 21 || idade > 55 || renda < 1000 || emprestimo < 500 || emprestimo > renda * 15){
     console.log("Recusado!");
 }
 else{
     console.log("Aprovado!");
     let parcelas = Number(prompt("Informe número de parcelas:"));
     let juros = 8.5 / 100;
-    let total = emprestimo * (1 + juros); // M = C x (1+ i)
-    if(parcelas <= 3){
-        console.log("O número de parcelas deve ser maior que 3.")
+    let total = emprestimo * ((1 + juros) ** parcelas); // fórmula juros composto: M = C (1+ i)**t 
+    if(parcelas < 3){
+        console.log("O número de parcelas deve ser maior que 3.");
     }
-    else if( parcelas >= 24){
-        console.log("O número de parcelas deve ser menor que 24.")
+    else if( parcelas > 24){
+        console.log("O número de parcelas deve ser menor que 24.");
     }
     else{
-        console.log(`O emprestimo no valor de R$${emprestimo}, dividido em ${parcelas} vezes com juros, terá um total de R$${total/parcelas} por mês!`);
+        console.log(`O emprestimo no valor de R$${emprestimo}, dividido em ${parcelas} vezes com juros, será de R$${(total/parcelas).toFixed(2)} por mês!`);
     }
 }
