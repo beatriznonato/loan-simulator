2ee1f41acea64760a8816932618dbcad0d9143d0
Beatriz <bia_nonato@icloud.com>
2018-09-27 23:24:33 -0300

    adicionar funcao de validacao

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+function validar(entrada, limiteInferior, limiteSuperior){
+    if(!limiteInferior){
+        limiteInferior = -Infinity;
+    }
+    
+    if(!limiteSuperior){
+        limiteSuperior = Infinity;
+    }
+    
+    if(entrada && !isNaN(Number(entrada)) && Number(entrada) >= limiteInferior && Number(entrada) <= limiteSuperior){
+        return true;
+    }
+    return false;
+}
+
 let idade = Number(prompt("Informe sua idade:"));
 let renda = Number(prompt("Informe sua renda:"));
 let emprestimo = Number(prompt("Informe valor do empréstimo:"));
 
-if(idade < 21 || idade > 55 || renda < 1000 || emprestimo < 500 || emprestimo > renda * 15){
+if((!validar(idade, 21, 55)) || (!validar(renda, 1000)) || (!validar(emprestimo, 500, renda * 15))){
+    console.log(validar(emprestimo, renda * 15));
     console.log("Recusado!");
 }
 else{
@@ -10,13 +26,13 @@ else{
     let parcelas = Number(prompt("Informe número de parcelas:"));
     let juros = 8.5 / 100;
     let total = emprestimo * ((1 + juros) ** parcelas); // fórmula juros composto: M = C (1+ i)**t 
-    if(parcelas < 3){
+    if((!validar(parcelas,3))){
         console.log("O número de parcelas deve ser maior que 3.");
     }
-    else if( parcelas > 24){
+    else if((!validar(parcelas,null,24))){
         console.log("O número de parcelas deve ser menor que 24.");
     }
     else{
-        console.log(`O emprestimo no valor de R$${emprestimo}, dividido em ${parcelas} vezes com juros, será de R$${(total/parcelas).toFixed(2)} por mês!`);
+        console.log(`O emprestimo no valor de R$${emprestimo}, dividido em ${parcelas} vezes com juros, será de R$${(total/parcelas).toFixed(2)} ao mês!`);
     }
 }
